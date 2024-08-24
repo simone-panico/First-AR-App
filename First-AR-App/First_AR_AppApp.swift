b9ed77b79e6f4736272b627bbca73515a2f4294c
Simone Panico <simone.panico@icloud.com>
2024-08-24 17:10:38 +0200

    Initial Commit

---
+++ First-AR-App/First_AR_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AR_AppApp.swift
+//  First-AR-App
+//
+//  Created by Simone Panico on 24.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct First_AR_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
